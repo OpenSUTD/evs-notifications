d98c316af6ea9a81eae5ae3bf50a5ee137f9b4ed
Gabriel Wong <gabrielwong159@gmail.com>
2019-05-20 12:59:24 +0800

    Add DB util functions for Telegram notifications

--- database/psql.py
+++ database/psql.py
@@ -1,5 +1,9 @@
+import sys
+sys.path.insert(0, '..')
+
 import psycopg2
 from collections import namedtuple
+import web
 
 
 def get_connection():
@@ -9,46 +13,7 @@ def get_connection():
     return conn
 
 
-def get_accounts():
-    query = """SELECT * FROM account;"""
-
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute(query)
-    rows = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
-    Account = namedtuple('Account', 'username,password,name')
-    return [Account(*row) for row in rows]
-
-
-def get_balances_by_username(username):
-    all_usernames = [acc.username for acc in get_accounts()]
-    if username not in all_usernames:
-        return []
-
-    query = f"""SELECT retrieve_date, amount
-                FROM balance
-                WHERE username = '{username}'
-                ORDER BY id;"""
-
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute(query)
-    rows = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
-
-    parse_date = lambda date: '{:04d}/{:02d}/{:02d}'.format(date.year, date.month, date.day)
-    return [(parse_date(date), amount) for date, amount in rows]
-
-
-def insert_balance(username, retrieve_date, amount):
-    query = f"""INSERT INTO balance (username, retrieve_date, amount) 
-                VALUES ('{username}', '{retrieve_date}', {amount});"""
-
+def execute_and_commit(query):
     conn = get_connection()
     cursor = conn.cursor()
     cursor.execute(query)
@@ -56,3 +21,94 @@ def insert_balance(username, retrieve_date, amount):
 
     cursor.close()
     conn.close()
+
+
+def execute_and_fetchall(query) -> list:
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+    return rows
+
+
+def get_accounts():
+    query = """SELECT * FROM account;"""
+    rows = execute_and_fetchall(query)
+    Account = namedtuple('Account', 'username password')
+    return [Account(*row) for row in rows]
+
+
+def username_valid(username):
+    """
+    Ensures that input username is valid (i.e. exists in database).
+
+    :param username: username of length 8 (by SUTD EVS standard)
+    :return: True if valid
+    """
+    all_usernames = [acc.username for acc in get_accounts()]
+    return username in all_usernames
+
+
+def get_balances_by_username(username):
+    if not username_valid(username):
+        return []
+
+    def parse_date(date):
+        return '{:04d}/{:02d}/{:02d}'.format(date.year, date.month, date.day)
+
+    query = f"""SELECT retrieve_date, amount
+                FROM balance
+                WHERE username = '{username}'
+                ORDER BY id;"""
+    rows = execute_and_fetchall(query)
+    return [(parse_date(date), amount) for date, amount in rows]
+
+
+def insert_balance(username, retrieve_date, amount):
+    query = f"""INSERT INTO balance (username, retrieve_date, amount) 
+                VALUES ('{username}', '{retrieve_date}', {amount});"""
+    execute_and_commit(query)
+
+
+def add_account(username: str, password: str) -> bool:
+    try:
+        web.get_amount(username, password)
+    except AssertionError:
+        return False
+
+    query = f"""INSERT INTO account (username, password)
+                VALUES ('{username}', '{password}')
+                ON CONFLICT DO NOTHING;"""
+    execute_and_commit(query)
+    return True
+
+
+def add_subscription(username: str, amount: str, chat_id: int) -> bool:
+    if not username_valid(username):
+        return False
+
+    query = f"""INSERT INTO subscription (username, amount, chat_id)
+                VALUES ('{username}', '{amount}', '{chat_id}')
+                ON CONFLICT DO NOTHING;"""
+    execute_and_commit(query)
+    return True
+
+
+def get_notifications():
+    query = """SELECT balance.username, balance.amount, chat_id
+               FROM subscription
+               INNER JOIN (
+                   SELECT *
+                   FROM balance
+                   WHERE balance.id IN (
+                       SELECT MAX (id) FROM balance GROUP BY username
+                   )
+               ) AS balance
+               ON balance.username = subscription.username
+               AND balance.amount <= subscription.amount;"""
+    rows = execute_and_fetchall(query)
+    Notification = namedtuple('Notification', 'username amount chat_id')
+    return [Notification(*row) for row in rows]
