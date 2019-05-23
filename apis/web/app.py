641efafb37a790ff1ffbaeb974c0774f43c77830
Gabriel Wong <gabrielwong159@gmail.com>
2019-05-24 01:22:00 +0800

    Reorganize APIs folder and implement web server, migration not done

---
+++ apis/web/app.py
@@ -0,0 +1,28 @@
+import json
+from flask import Flask, request
+from operator import itemgetter
+from web import login_valid, get_amount
+
+app = Flask(__name__)
+
+
+@app.route('/validate', methods=['POST'])
+def validate():
+    body = request.get_json()
+    username, password = itemgetter('username', 'password')(body)
+    return json.dumps({
+        'valid': login_valid(username, password)
+    })
+
+
+@app.route('/credit', methods=['POST'])
+def credit():
+    body = request.get_json()
+    username, password = itemgetter('username', 'password')(body)
+    return json.dumps({
+        'amount': get_amount(username, password)
+    })
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8001)
--- web/main.py
+++
@@ -1,5 +0,0 @@
-import scraper
-
-
-if __name__ == '__main__':
-    scraper.add_all_balances()
