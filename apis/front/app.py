9ebf65080404f7fa2fa70e31018bc66c63bae00c
Gabriel Wong <gabrielwong159@gmail.com>
2019-05-25 00:01:03 +0800

    Modify front end API to previous endpoint, add CORS

--- apis/front/app.py
+++ apis/front/app.py
@@ -1,13 +1,15 @@
 import requests
 from flask import Flask
+from flask_cors import CORS
 
 DB_API_HOST = 'localhost'
 DB_API_PORT = 8001
 
 app = Flask(__name__)
+CORS(app)
 
 
-@app.route('/balance/username/<username>')
+@app.route('/balance/<username>')
 def get_balances_by_username(username):
     url = f'http://{DB_API_HOST}:{DB_API_PORT}/balance/username/{username}'
     r = requests.get(url)
