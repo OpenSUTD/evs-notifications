a55d3ee3a190cda32e66417ddb78950a6df7dbbc
Gabriel Wong <gabrielwong159@gmail.com>
2019-06-09 14:12:24 +0800

    Edit regex for web scraper to include amounts above $100

--- apis/web/web.py
+++ apis/web/web.py
@@ -25,6 +25,15 @@ def login_valid(username: str, password: str) -> bool:
     return 'Invalid' not in r.url
 
 
+def get_balance_from_text(text):
+    pattern = r'S\$ ((\d{1,3},)*\d{1,3}\.\d{2})'  # S$ (ddd,)*ddd.dd
+    matches = re.findall(pattern, text)
+    assert len(matches) > 1, 'Could not find balance on page'
+
+    match = matches[1][0].replace(',', '')
+    return float(match)
+
+
 def get_amount(username, password):
     sess = requests.session()
     data, headers = get_request_params(username, password)
@@ -35,8 +44,6 @@ def get_amount(username, password):
     r = sess.get(credit_url)
     assert r.status_code == 200, 'Could not retrieve credit page'
 
-    pattern = r'S\$ (\d{1,2}\.\d{2})'  # S$ (d.dd) OR S$ (dd.dd)
-    matches = re.findall(pattern, r.text)
-    assert len(matches) > 1, 'Could not find balance on page'
+    amount = get_balance_from_text(r.text)
+    return amount
 
-    return float(matches[1])
