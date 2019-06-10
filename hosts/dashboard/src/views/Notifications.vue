da0bc7a29b27b6711277495f7e6770c08cecb879
Gabriel Wong <gabrielwong159@gmail.com>
2019-06-10 12:25:17 +0800

    Add link to Telegram bot on Notifications

--- hosts/dashboard/src/views/Notifications.vue
+++ hosts/dashboard/src/views/Notifications.vue
@@ -2,6 +2,14 @@
   <div id="notifications">
     <h1 class="mb-3">Notifications</h1>
     <p>Set up Telegram notifications when your credit balance falls below a preset threshold.</p>
+    <a class="ma-3" href="https://t.me/evs_notification_bot">
+    	<v-hover>
+	    	<img 
+	    		slot-scope="{ hover }" 
+	    		:class="`elevation-${hover ? 4 : 0}`"
+	    		src="https://telegram.org/img/t_logo.png">
+	    </v-hover>
+    </a>
   </div>
 </template>
 
@@ -18,4 +26,14 @@ export default {
   margin: auto;
   padding-top: 60px;
 }
+
+a {
+	display: inline-block;
+}
+
+img {
+	height: 128px;
+	border-radius: 50%;
+}
+
 </style>
