1b9475c970bbe83ee4fdecd8190e733ee00a3bab
Gabriel Wong <gabrielwong159@gmail.com>
2019-06-25 15:24:34 +0800

    Refactor web API util

--- apis/web/web.py
+++ apis/web/web.py
@@ -1,11 +1,16 @@
 import re
 import requests
 
-login_url = 'https://nus-utown.evs.com.sg/SUTDMain/loginServlet'
-credit_url = 'https://nus-utown.evs.com.sg/SUTDMain/viewMeterCreditServlet'
+base_url = 'https://nus-utown.evs.com.sg/SUTDMain'
+login_url = f'{base_url}/loginServlet'
+credit_url = f'{base_url}/viewMeterCreditServlet'
 
 
 def get_request_params(username: str, password: str):
+    if type(username) != str:
+        raise TypeError('Username must be type "string"')
+    if type(password) != str:
+        raise TypeError('Password must be type "string"')
     data = {
         'txtLoginId': username,
         'txtPassword': password,
@@ -16,13 +21,14 @@ def get_request_params(username: str, password: str):
 
 
 def login_valid(username: str, password: str) -> bool:
-    sess = requests.session()
-    data, headers = get_request_params(username, password)
-    r = sess.post(url=login_url, data=data, headers=headers)
-    # if invalid, r.url will be
-    # https://nus-utown.evs.com.sg/SUTD/index.jsp?
-    # strMsg=Invalid%20Login%20ID%20or%20Password.
-    return 'Invalid' not in r.url
+    with requests.session() as sess:
+        data, headers = get_request_params(username, password)
+        r = sess.post(url=login_url, data=data, headers=headers)
+        # if invalid, r.url will be
+        # https://nus-utown.evs.com.sg/SUTD/index.jsp?
+        # strMsg=Invalid%20Login%20ID%20or%20Password.
+        is_valid = 'Invalid' not in r.url
+    return is_valid
 
 
 def get_balance_from_text(text):
@@ -35,15 +41,15 @@ def get_balance_from_text(text):
 
 
 def get_amount(username, password):
-    sess = requests.session()
-    data, headers = get_request_params(username, password)
+    with requests.session() as sess:
+        data, headers = get_request_params(username, password)
 
-    r = sess.post(url=login_url, data=data, headers=headers)
-    assert r.status_code == 200, 'Could not login'
+        r = sess.post(url=login_url, data=data, headers=headers)
+        assert r.status_code == 200, 'Could not login'
 
-    r = sess.get(credit_url)
-    assert r.status_code == 200, 'Could not retrieve credit page'
+        r = sess.get(credit_url)
+        assert r.status_code == 200, 'Could not retrieve credit page'
 
-    amount = get_balance_from_text(r.text)
+        amount = get_balance_from_text(r.text)
     return amount
 
