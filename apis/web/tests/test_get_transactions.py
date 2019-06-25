24b970a1d7e3759ca3f66e83407b62ff78ec1d60
Gabriel Wong <gabrielwong159@gmail.com>
2019-06-25 17:33:58 +0800

    Add web API utils to retrieve transaction history

---
+++ apis/web/tests/test_get_transactions.py
@@ -0,0 +1,56 @@
+import json
+import unittest
+from bs4 import BeautifulSoup
+from web.transaction import Transaction, get_transactions, row_to_transaction, transaction_to_dict
+
+
+def read_credentials(filename='tests/credentials.json'):
+    with open(filename) as f:
+        text = f.read()
+    obj = json.loads(text)
+    return obj['username'], obj['password']
+
+
+class TestGetTransactions(unittest.TestCase):
+    def test_row_to_transaction(self):
+        data = ('<tr>'
+                '  <td>transaction_id</td>'
+                '  <td>date</td>'
+                '  <td>amount</td>'
+                '  <td>offer_id</td>'
+                '  <td>payment_mode</td>'
+                '  <td>channel</td>'
+                '  <td>status</td>'
+                '</tr>')
+        soup = BeautifulSoup(data, 'html.parser')
+        result = row_to_transaction(soup.tr)
+
+        txn = Transaction(transaction_id='transaction_id',
+                          date='date',
+                          amount='amount',
+                          offer_id='offer_id',
+                          payment_mode='payment_mode',
+                          channel='channel',
+                          status='status')
+        self.assertEqual(result, txn)
+
+    def test_transaction_to_dict(self):
+        data = Transaction(date='01/01/2019 00:00', amount='10.00',
+                           transaction_id=None, offer_id=None, payment_mode=None, channel=None, status=None)
+        result = transaction_to_dict(data)
+        self.assertEqual(result, {'date': '2019-01-01', 'amount': 10})
+
+    def test_get_transactions(self):
+        username, password = read_credentials()
+        transactions = get_transactions(username, password)
+        self.assertEqual(type(transactions), list)
+
+    def test_get_transactions_attributes(self):
+        username, password = read_credentials()
+        transactions = get_transactions(username, password)
+        for txn in transactions:
+            self.assertEqual(type(txn), dict)
+
+            date, amount = txn['date'], txn['amount']
+            self.assertEqual(type(date), str)
+            self.assertEqual(type(amount), float)
