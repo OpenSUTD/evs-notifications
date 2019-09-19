42edfed85be2041111a0b1ccbddd2f9dea019041
Gabriel Wong <gabrielwong159@gmail.com>
2019-09-19 23:04:46 +0800

    Modify dashboard date filter to use last date from balances
    Previously used current date
    Change to use balances to work with demo balances

--- hosts/dashboard/src/views/utils/data.js
+++ hosts/dashboard/src/views/utils/data.js
@@ -1,3 +1,9 @@
+function getLastDate(balances) {
+	let lastBalance = balances[balances.length - 1];  // assume chronological order of balances
+	let lastDate = lastBalance[0];
+	return new Date(lastDate);
+}
+
 function filterBalancesByDate(balances, earliestDate) {
 	let dateFilter = (t) => Date.parse(t[0]) >= earliestDate;
 	return balances.filter(dateFilter);
@@ -5,14 +11,14 @@ function filterBalancesByDate(balances, earliestDate) {
 
 module.exports = {
 	getPastWeekBalances: (balances) => {
-		let filterDate = new Date();
+		let filterDate = getLastDate(balances);
 		filterDate.setHours(0, 0, 0, 0);
 		filterDate.setDate(filterDate.getDate() - 7);
 		return filterBalancesByDate(balances, filterDate);
 	},
 
 	getPastMonthBalances: (balances) => {
-		let filterDate = new Date();
+		let filterDate = getLastDate(balances);
 		filterDate.setHours(0, 0, 0, 0);
 		filterDate.setMonth(filterDate.getMonth() - 1);
 		return filterBalancesByDate(balances, filterDate);
