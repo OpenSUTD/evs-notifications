b457dcbe6875d447e58a1ec7e0bf24db94c7b36d
Gabriel Wong <gabrielwong159@gmail.com>
2021-10-19 20:14:21 +0800

    Update HTTPS proxy script with new EC2 instance IP
    IP address changes when EC2 instance is stopped and started, instead of rebooted.

--- apis/front/proxy.py
+++ apis/front/proxy.py
@@ -2,7 +2,7 @@ import requests
 from flask import Flask, request
 from flask_cors import CORS
 
-HOST = 'http://18.138.243.186:8000'
+HOST = 'http://18.141.215.88:8000'
 app = Flask(__name__)
 CORS(app)
 
